883b1debb4b5f553fd2741894723b5600e4efdd9
connorabernethy <cjaberne@asu.edu>
2021-12-01 10:54:32 -0700

    Solved Part One.

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+/*
+*/
+
+
+namespace Day1{
+    class Day1{
+
+        static int totalCount = 0;
+        static void Main(string[] args)
+        {
+            var answer = SolvePartOne();
+        }
+
+        static int SolvePartOne(){
+            var listOfInts = File.ReadAllLines("input.txt")
+            .Select(s => Int32.Parse(s))
+            .ToList();
+            for (int i = 0; i < listOfInts.Count - 1; i++)
+            {
+                if (listOfInts[i+1] > listOfInts[i])
+                {
+                    totalCount++;
+                }
+            }
+            return totalCount;
+        }
+    }
+}
