c269adf7fb67efe6ec9c73a1815adcc4ab69e446
connorabernethy <cjaberne@asu.edu>
2021-12-02 12:18:47 -0700

    dotnet new console -o "day2"

---
+++ day2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+/* 
+/ Connor Abernethy
+/ 12/2/2021
+/ Advent of Code 2021
+*/
+
+
+namespace Day2{
+    class Day2{
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
