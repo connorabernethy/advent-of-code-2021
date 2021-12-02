0d8b76e93e72b814cd4ccb91ddc10c62ca195bbc
connorabernethy <cjaberne@asu.edu>
2021-12-02 12:44:04 -0700

    Solved Part One.

--- day2/Program.cs
+++ day2/Program.cs
@@ -10,9 +10,27 @@ using System.Linq;
 
 namespace Day2{
     class Day2{
-
+        static int horizontalPosition, depth = 0;
         static void Main(string[] args)
         {
+            var list = File.ReadAllLines("input.txt").ToList();
+            for (int i = 0; i < list.Count; i++){
+                if (list[i].Contains("forward")){
+                    // Get the last digit at the end of the string.
+                    var forward = Int32.Parse(list[i].ElementAt(list[i].Length - 1).ToString());
+                    horizontalPosition += forward;
+                }
+                else if (list[i].Contains("up")){
+                    var up = Int32.Parse(list[i].ElementAt(list[i].Length - 1).ToString());
+                    depth -= up;
+                }
+                else{
+                    var down = Int32.Parse(list[i].ElementAt(list[i].Length - 1).ToString());
+                    depth += down;
+                }
+            }
+
+            Console.WriteLine("Answer: " + (depth * horizontalPosition));
         }
     }
 }
