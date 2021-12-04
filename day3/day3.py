25dcc7aa62dae7167cdf97d0eb72f31019d95c8a
connorabernethy <cjaberne@asu.edu>
2021-12-04 08:32:32 -0700

    Solved Part Two in Python !

--- day3/day3.py
+++ day3/day3.py
@@ -1,3 +1,7 @@
+# Connor Abernethy
+# Advent of Code 2021
+# 12/03/2021
+
 def part_one():
     f = open("input.txt")
 
@@ -34,4 +38,57 @@ def part_one():
     # Strings converted to decimal values and multiplied together:
     print(199 * 3896)
 
+def part_two():
+    f = open("input.txt")
+
+    list = f.readlines()
+    newList = list
+
+    count = 0
+    index = 0
+    oxygenRating = ""
+
+    while index < len(list[0]) - 1:
+        for num in newList:
+            if num[index] == "1":
+                count += 1
+            else:
+                count -= 1
+        if count > 0 or count == 0:
+            newList = [num1 for num1 in newList if num1[index] != "0"]
+        elif count < 0:
+            newList = [num1 for num1 in newList if num1[index] != "1"]
+
+        count = 0
+        index += 1
+
+    oxygenRating = newList[0]
+    newList = list
+    co2Rating = ""
+    index = 0
+
+    while index < len(list[0]) - 1:
+        for num in newList:
+            if num[index] == "1":
+                count += 1
+            else:
+                count -= 1
+        if count > 0 or count == 0:
+            newList = [num1 for num1 in newList if num1[index] != "1"]
+        elif count < 0:
+            newList = [num1 for num1 in newList if num1[index] != "0"]
+        
+        if len(newList) == 1:
+            co2Rating = newList[0]
+
+        count = 0
+        index += 1
+
+    print("Oxygen rating: " + oxygenRating)
+    print("CO2 rating: " + co2Rating)
+    # Binary ratings converted to decimal then multiplied
+    print("Answer: ", end="")
+    print((509*2693))
+
 part_one()
+part_two()
