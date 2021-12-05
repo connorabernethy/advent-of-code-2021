daf0e2ec9f5df2536af3bb8e9391eabb69ce8058
connorabernethy <cjaberne@asu.edu>
2021-12-04 19:35:51 -0700

    Solved part one.

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,13 @@
-f = open("input.txt")
+def check_board(board):
+    total = 0
+    for num in board:
+        if num[1] == False:
+            total += num[0]
+    return total
 
+
+f = open("input.txt")
+total = 0
 list = f.readlines()
 
 print(len(list))
@@ -56,9 +64,47 @@ for number in inputList:
         while (index < len(board)):
             if board[index][0] == number:
                 board[index][1] = True
+                if board[0][1] == True and board[1][1] == True and board[2][1] == True and board[3][1] == True and board[4][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[5][1] == True and board[6][1] == True and board[7][1] == True and board[8][1] == True and board[9][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[10][1] == True and board[11][1] == True and board[12][1] == True and board[13][1] == True and board[14][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[15][1] == True and board[16][1] == True and board[17][1] == True and board[18][1] == True and board[19][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[20][1] == True and board[21][1] == True and board[22][1] == True and board[23][1] == True and board[24][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[0][1] == True and board[5][1] == True and board[10][1] == True and board[15][1] == True and board[20][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[1][1] == True and board[6][1] == True and board[11][1] == True and board[16][1] == True and board[21][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[2][1] == True and board[7][1] == True and board[12][1] == True and board[17][1] == True and board[22][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[3][1] == True and board[8][1] == True and board[13][1] == True and board[18][1] == True and board[23][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
+                elif board[4][1] == True and board[9][1] == True and board[14][1] == True and board[19][1] == True and board[24][1] == True:
+                    win = board[index][0]
+                    total = check_board(board)
+                    print(total * win)
 
             index +=1
     index = 0
     count = 0
-    total = 0
-    print(boards)
